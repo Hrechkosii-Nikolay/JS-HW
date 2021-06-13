e8515ba95031de9c1d9db6115d49a599ba9e8474
Hrechkosii-Nikolay <60157425+Hrechkosii-Nikolay@users.noreply.github.com>
2021-06-13 21:03:22 +0300

    fix

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,38 +1,39 @@
-import cardsMarkup from "./template/menuCardMarkup.hbs";
-import menu from "./menu.json";
+import './sass/main.scss';
+import cardsMarkup from './template/menuCardMarkup.hbs';
+import menu from './menu.json';
 
-const menuRef = document.querySelector(".js-menu");
+const menuRef = document.querySelector('.js-menu');
 
 const menuMarkup = cardsMarkup(menu);
-menuRef.insertAdjacentHTML("beforeend", menuMarkup);
+menuRef.insertAdjacentHTML('beforeend', menuMarkup);
 
 const Theme = {
-  LIGHT: "light-theme",
-  DARK: "dark-theme",
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
 
-const themeBtnRef = document.querySelector("#theme-switch-toggle");
-const bodyRef = document.querySelector("body");
-const carrentTheme = localStorage.getItem("theme");
+const themeBtnRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+const carrentTheme = localStorage.getItem('theme');
 
 bodyRef.classList.add(Theme.LIGHT);
 
-themeBtnRef.addEventListener("change", onCliktChengeThemeBtn);
+themeBtnRef.addEventListener('change', onCliktChengeThemeBtn);
 
 function onCliktChengeThemeBtn() {
   if (themeBtnRef.checked) {
     bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
-    localStorage.setItem("theme", Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
   } else {
     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
-    localStorage.setItem("theme", Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
   }
 }
 
 function saveCarrentTheme() {
   if (carrentTheme === Theme.DARK) {
     bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
-    themeBtnRef.setAttribute("checked", true);
+    themeBtnRef.setAttribute('checked', true);
   }
 }
 saveCarrentTheme();
